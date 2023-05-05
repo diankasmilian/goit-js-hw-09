6f7673d7865a8a9fe10be8a9b67d8323818e3ddc
Diana Smilian <dianka.kulakevich@gmail.com>
2023-05-06 01:22:43 +0300

    finish task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
 
+
+const refs = {
+   body: document.querySelector('body'),
+   btnStart: document.querySelector('button[data-start]'),
+   btnStop: document.querySelector('button[data-stop]')
+};
+
+let intervalId;
+let isOnColorSwitcher = false;
+
+refs.btnStart.addEventListener('click', onStart);
+refs.btnStop.addEventListener('click', onStop);
+
+function onStart () {
+   refs.btnStart.setAttribute("disabled", "");
+   if (isOnColorSwitcher) {
+      return;
+   }
+   isOnColorSwitcher = true;
+
+   intervalId = setInterval(() => {
+      refs.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+      
+};
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+ };
+
+ function onStop () {
+   clearInterval(intervalId);
+   refs.btnStart.removeAttribute("disabled");
+   isOnColorSwitcher = false;
+ }
