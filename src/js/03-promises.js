49f1a31767dd4a56a8210062b17e2a10d403da07
Diana Smilian <dianka.kulakevich@gmail.com>
2023-05-06 18:40:35 +0300

    start task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,11 @@
+const refs = {
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+  form: document.querySelector('.form'),
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
