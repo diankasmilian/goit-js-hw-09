f71277f7eb9ab6382816e461e55c172289d01c8c
diankasmilian <119013441+diankasmilian@users.noreply.github.com>
2023-05-07 12:27:01 +0000

    Deploying to gh-pages from @ diankasmilian/goit-js-hw-09@bc5eef3354230e77e9b2ac13c9611681c3ece778 🚀

--- 03-promises.7b8fe16b.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("input[name=delay]"),document.querySelector("input[name=step]"),document.querySelector("input[name=amount]"),document.querySelector(".form");
-//# sourceMappingURL=03-promises.7b8fe16b.js.map
