bc5eef3354230e77e9b2ac13c9611681c3ece778
Diana Smilian <dianka.kulakevich@gmail.com>
2023-05-07 15:26:29 +0300

    finish task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,41 @@
 const refs = {
-  delay: document.querySelector('input[name=delay]'),
-  step: document.querySelector('input[name=step]'),
-  amount: document.querySelector('input[name=amount]'),
   form: document.querySelector('.form'),
-}
+};
+
+refs.form.addEventListener('submit', onSubmitForm);
 
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+ 
+  return new Promise ((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setInterval(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      };
+    }, delay);
+  });
+};
+
+function onSubmitForm (e) {
+e.preventDefault();
+
+const delay = Number(e.target.elements.delay.value);
+  const step = Number(e.target.elements.step.value);
+  const amount = Number(e.target.elements.amount.value);
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const currentDelay = delay + i * step;
+    createPromise(position, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
-}
+};
